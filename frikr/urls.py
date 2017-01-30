eb7fe7cba3e044ba462f017672d2fed21ee1f406
erniker <josepablomedinagrande@hotmail.com>
2017-01-30 19:16:44 +0100

    19:16 30/01/2017 PC

--- frikr/urls.py
+++ frikr/urls.py
@@ -19,16 +19,17 @@ from django.conf.urls import url
 from django.contrib import admin
 from photos import views as photos_views
 from users import views as users_views
-from photos.views import HomeView, DetailView, CreateView, ListView
+from photos.views import HomeView, DetailView, CreateView, PhotoListView, UserPhotosView
 from users.views import LoginView, LogoutView
-
+from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # Photo URLs
     url(r'^$', HomeView.as_view(), name='photos_home'),
-    url(r'^photos/$', ListView.as_view(), name='photos_list'),
+    url(r'^my-photos/$', login_required(UserPhotosView.as_view()), name='user_photos'),
+    url(r'^photos/$', PhotoListView.as_view(), name='photos_list'),
     url(r'^photos/(?P<pk>[0-9]+)$', DetailView.as_view(), name='photo_detail'),
     url(r'^photos/new$', CreateView.as_view(), name='create_photo'),
 
