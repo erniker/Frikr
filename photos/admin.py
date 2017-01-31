6f16f26b128917afcb68e73e3279175cf2eedade
erniker <josepablomedinagrande@hotmail.com>
2017-01-31 21:40:45 +0100

    21:40 31/01/2017 PC

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from photos.models import Photo
 
+
+class PhotoAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'owner_name', 'license', 'visibility')
+    list_filter = ('license', 'visibility')
+    search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + u' ' + obj.owner.last_name
+    owner_name.short_description = u'Photo owner'
+    owner_name.admin_order_field = 'owner'
+
 # Register your models here.
-admin.site.register(Photo)
+admin.site.register(Photo, PhotoAdmin)
