a1865f6421f6c15ac5e8d0771b13063180859448
erniker <josepablomedinagrande@hotmail.com>
2017-02-01 11:58:46 +0100

    12.00 01/02/2017 Portatil

--- photos/admin.py
+++ photos/admin.py
@@ -13,5 +13,20 @@ class PhotoAdmin(admin.ModelAdmin):
     owner_name.short_description = u'Photo owner'
     owner_name.admin_order_field = 'owner'
 
+    fieldsets = (
+        (None, {
+            'fields': ('name',),
+            'classes': ('wide',)
+        }),
+        ('Description & Author', {
+            'fields': ('description', 'owner'),
+            'classes': ('wide',)
+        }),
+        ('Extra', {
+            'fields': ('url', 'license', 'visibility'),
+            'classes': ('wide', 'collapse')
+        })
+    )
+
 # Register your models here.
 admin.site.register(Photo, PhotoAdmin)
