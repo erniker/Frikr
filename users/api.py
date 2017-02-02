d10a1c74f502de1ad9e88ab17fef6699e99c9a35
erniker <josepablomedinagrande@hotmail.com>
2017-02-02 21:43:53 +0100

    PC

--- users/api.py
+++ users/api.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-from django.views.generic import View
+
 from django.contrib.auth.models import User
 from users.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-class UserListAPI(View):
+class UserListAPI(APIView):
 
     def get(self, request):
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
         serialized_users = serializer.data  #lista de diccionarios
-        renderer = JSONRenderer()
-        json_users = renderer.render(serialized_users)  #lista de diccionarios ->JSON
-        return HttpResponse(json_users)
+        return Response(serialized_users)
+
 
