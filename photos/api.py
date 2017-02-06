b6c6d5ded921bfa2270aa3f008bce19e6c075523
erniker <josepablomedinagrande@hotmail.com>
2017-02-06 20:54:52 +0100

    20:53 06/02/2017 PC

--- photos/api.py
+++ photos/api.py
@@ -2,16 +2,26 @@
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from photos.views import PhotosQueryset
 
-
-class PhotoListAPI(ListCreateAPIView):
+class PhotoListAPI(PhotosQueryset, ListCreateAPIView):
     queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoSerializer
 
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
 
-class PhotoDetailtAPI(RetrieveUpdateDestroyAPIView):
+
+class PhotoDetailtAPI(PhotosQueryset, RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
+
 
